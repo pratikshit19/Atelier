d41a6d388d89c962be33e1b3ee960fe2b5bb305f
pratikshit19 <pratikshit@icloud.com>
2026-04-19 18:11:36 +0530

    Made this into a PWA

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'atelier-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/screen.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
